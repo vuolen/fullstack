2cfb7ac71cc4fdee4cd28cf9abc52847fad9ed63
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-02-04 16:47:28 +0200

    4.19

--- osa4/bloglist/src/controllers/blogs.js
+++ osa4/bloglist/src/controllers/blogs.js
@@ -1,5 +1,6 @@
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 
 blogsRouter.get('/', async (request, response) => {
@@ -11,7 +12,23 @@ blogsRouter.get('/', async (request, response) => {
         })
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')  
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)  
+    } 
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
+    const token = getTokenFrom(request)
+    decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({error: "token missing or invalid"})
+    }
+
+    const user = await User.findById(decodedToken.id)
+
     const blog = new Blog(request.body)
 
     if (blog.likes === undefined) {
@@ -22,7 +39,6 @@ blogsRouter.post('/', async (request, response) => {
         return
     }
 
-    const user = await User.findOne({})
     blog.user = user._id
 
     const savedBlog = await blog.save()
