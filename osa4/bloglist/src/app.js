1a128df0299ca711f17cbeba55d5c7f7de4bfb92
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-02-04 16:17:16 +0200

    4.18

--- osa4/bloglist/src/app.js
+++ osa4/bloglist/src/app.js
@@ -13,8 +13,10 @@ app.use(express.json())
 
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
 
 module.exports = app
