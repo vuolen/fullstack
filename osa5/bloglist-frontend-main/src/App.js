3e3f215d6c743d5a9b064057fb1ac99c95b3921c
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-03-20 16:46:02 +0200

    5.1

--- osa5/bloglist-frontend-main/src/App.js
+++ osa5/bloglist-frontend-main/src/App.js
@@ -1,11 +1,21 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username]
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const response = await axios.post("/api/login", {username, password})
+    setUser(response.data)
+    setUsername("")
+    setPassword("")
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -17,9 +27,9 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
-        <form action='/api/login' method="POST">
-          username: <input type="text" id="username"></input><br/>
-          password: <input type="password" id="password"></input><br/>
+        <form onSubmit={handleLogin}>
+          username: <input type="text" id="username" onChange={({target}) => setUsername(target.value)}></input><br/>
+          password: <input type="password" id="password" onChange={({target}) => setPassword(target.value)}></input><br/>
           <input type="submit" value="Log in"></input>
         </form>
       </div>
@@ -29,6 +39,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      {user.name} logged in
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
