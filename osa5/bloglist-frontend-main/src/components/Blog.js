94b97658d12769c136492b1cf751ac33142ea382
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-03-22 17:00:39 +0200

    5.7

--- osa5/bloglist-frontend-main/src/components/Blog.js
+++ osa5/bloglist-frontend-main/src/components/Blog.js
@@ -1,7 +1,24 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+  const blogStyle = {
+    margin: "10px"
+  }
+  return (<div style={blogStyle}>
+    {blog.title} 
+    <button onClick={() => setExpanded(!expanded)}>
+      {expanded ? "hide" : "view"} 
+    </button>
+    <br/>
+    {expanded ? 
+      <>
+        {blog.author} <br/>
+        {blog.url} <br/>
+        {blog.likes} <br/>
+      </>
+      : null}
+  </div>)
+}
 
 export default Blog
