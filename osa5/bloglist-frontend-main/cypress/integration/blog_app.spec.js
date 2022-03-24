3be47da006aba11eb1762e8bd61efd76ab3181e7
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-03-24 11:08:29 +0200

    5.18

--- osa5/bloglist-frontend-main/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend-main/cypress/integration/blog_app.spec.js
@@ -8,6 +8,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request("POST", "http://localhost:3003/api/users", {
+        name: "Test Testersson",
+        username: "testuser",
+        password: "testpassword"
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -15,5 +20,21 @@ describe('Blog app', function() {
     cy.contains("Log in")
     cy.get("input#username")
     cy.get("input#password")
+  })
+
+  describe("Login", function() {
+    it('succeeds with correct credentials', function() {
+        cy.get("input#username").type("testuser")
+        cy.get("input#password").type("testpassword")
+        cy.get("form").submit()
+        cy.contains("Test Testersson logged in")
+    })
+
+    it('fails with wrong credentials', function() {
+        cy.get("input#username").type("testuser")
+        cy.get("input#password").type("wrongpassword")
+        cy.get("form").submit()
+        cy.contains("invalid username or password")
+    })
   })
 })
